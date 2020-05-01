878fbbe614cf253273799980bdd765d88a87c943
Tess2019 <terese_burman@hotmail.com>
2020-05-01 06:42:44 +0000

    the overlay text function

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+document.addEventListener("DOMContentloaded", ready ());
 
+function ready(){
+    let overlays = Array.from (document.getElementsByClassName ("overlay-text")); /*without array its just html*/
+    let cards =Array.from (document.getElementsByClassName("card"));
+
+    overlays.forEach(overlay =>{
+        overlay.addEventListener("click", () => {
+          overlay.classlist.remove ("visable");  
+        })
+    })
+
+}
+
+
+
+const card = document.querySelectorAll(".card");
+
+
+
+
+
+
+
+
+card.forEach(card => {
+    card.addEventListener("click", () => {
+
+    });
+});
