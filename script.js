773525bf4a4a2073118cd21ed0ed61c683c3f067
Tess2019 <terese_burman@hotmail.com>
2020-05-03 10:13:38 +0000

    comment out the not current working functions

--- script.js
+++ script.js
@@ -13,30 +13,28 @@ function ready(){
 }
 
 
-const card = document.querySelectorAll(".card");
+const cards = document.querySelectorAll(".card");
 
 //memory board length and add flipcard later  
-function memoryBoard(){//0-12
+//function memoryBoard(){}0-12
     
-    }
+    
 
-function checkForMatch(){
+//function checkForMatch(){}
 
-}
+
 let flippedCard = false;
 let firstCard , secondCard;
 
 //add first and second card, witch card flips
  function flipcard () {
-     if (!flippedCard) { //first card
+     if (!flippedCard) { //first card click
         flippedCard = true;
         firstCard = this; //.card
         this.classlist.add ("flip");
         console.log ({flippedCard , firstCard});
-
-
-    }
+     }
     
 
  }
-cards .forEach (card => card .addEventListener("click", flipCard));
+cards.forEach (card => card .addEventListener("click", flipCard));
