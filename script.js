5a26ef945ae1aa5841a675b926ca0519efa6da1c
Tess2019 <terese_burman@hotmail.com>
2020-05-03 09:54:24 +0000

    start over

--- script.js
+++ script.js
@@ -2,26 +2,41 @@ document.addEventListener("DOMContentloaded", ready ());
 
 function ready(){
     let overlays = Array.from (document.getElementsByClassName ("overlay-text")); /*without array its just html*/
-    let cards =Array.from (document.getElementsByClassName(".card"));
+    let card = Array.from (document.getElementsByClassName(".card"));
 
     overlays.forEach(overlay =>{
         overlay.addEventListener("click", () => {
           overlay.classlist.remove ("visable");  
-        })
-    })
+        });
+    });
 
 }
 
 
-
 const card = document.querySelectorAll(".card");
 
-
- 
-
-
-card.forEach(card => {
-    card.addEventListener("click");
+//memory board length and add flipcard later  
+function memoryBoard(){//0-12
     
-});
+    }
 
+function checkForMatch(){
+
+}
+let flippedCard = false;
+let firstCard , secondCard;
+
+//add first and second card, witch card flips
+ function flipcard () {
+     if (!flippedCard) { //first card
+        flippedCard = true;
+        firstCard = this; //.card
+        this.classlist.add ("flip");
+        console.log ({flippedCard , firstCard});
+
+
+    }
+    
+
+ }
+cards .forEach (card => card .addEventListener("click", flipCard));
