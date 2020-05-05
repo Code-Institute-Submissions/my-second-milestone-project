3ec93cb9321cd695ff61809e5f288c8c57f05fe5
Tess2019 <terese_burman@hotmail.com>
2020-05-05 21:31:35 +0000

    another shuffle algorithm

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 $(document).ready(function() {
 
+    
     $(".card").on("click", function(){ 
-        $(this).css("img");
+    
 
 
         });
 
         
 
-    });
-
-    $("#startButton").on("click", function(){
-        $(".card-front").length();
-        
+    
+    
 
 
+   /* $("#startButton").on("click", function(){ 
+        $(".card").click().each();
+        for (let i = this.card.length -1; i > 0; i--){
+            let randIndex = Math.floor(Math.random() * (i + 1));//rounding down
+            this.card [randIndex].style.order = i; // order property check algorithm
+            this.card [i].style.order = randIndex;
+        }
         
-        
-        
-
-        
-    });
+        });*/
+    }); 
