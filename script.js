537e2fea2ef0f5875a409b5edf605a70867db973
Tess2019 <terese_burman@hotmail.com>
2020-05-05 09:40:02 +0000

    added startbutton and card click function

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-(document).ready(function() 
+$(document).ready(function() {
+
+    $(".card").click(function() {
+        $(this).trigger();
+        
+
+    });
+
+    $("#startButton").on("click", function(){
+        $(".card").content();{
+            $.each (this. get(), function(index, el) { //el reference object
+                var $el = $(el);
+                var $find =$el.children();
+
+                $find.sort(function(){
+                    return 0.5 - Math.random();
+                });
+                $el.empty ();
+                $find.appendTo($el);
+            });
+        }
+
+        
+    });
+});
