4c13d86df9e2949f5ea5df60c3d8c9bfbe4bf414
Tess2019 <terese_burman@hotmail.com>
2020-05-06 07:13:14 +0000

    start on click and time

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
 
     
     $(".card").on("click", function(){ 
+        $(".card-front").show (2000);
+        console.log ("done");
     
+ });
+ 
 
-
-        });
 
         
 
@@ -14,7 +16,7 @@ $(document).ready(function() {
 
 
    /* $("#startButton").on("click", function(){ 
-        $(".card").click().each();
+        $(".card").find(".card-front");
         for (let i = this.card.length -1; i > 0; i--){
             let randIndex = Math.floor(Math.random() * (i + 1));//rounding down
             this.card [randIndex].style.order = i; // order property check algorithm
