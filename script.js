02568eea530856088cac18a2524e37dd4205f6c9
Tess2019 <terese_burman@hotmail.com>
2020-05-06 18:42:45 +0000

    reperation progrss started

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+   // $(".card").on("click", function(){
+        //$("this").click ();
+    
+   // })
+$("#startButton").click(function(){
+$(this).css("color", green);
+});
+
+
+//shuffle cards
+
+//match cards
+
+//win
+
+//loose
+
+//reset/start over
+
+
+
+
+   // $(".memory-container").class("totalTime, cards");
+   // $(this).each("cards");
+
+
+})
