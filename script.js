9470cd558a522bb545b12a3bd5e67a24821eb4a9
Tess2019 <terese_burman@hotmail.com>
2020-05-13 11:27:53 +0000

    card flip

--- script.js
+++ script.js
@@ -1,57 +1,10 @@
-$(document).ready(function(){
-
-/*var myArray = { cards: ["wheel-robot.png","dog-robot.png","flying-robot.png","excited-robot.png","invader-zim-robot.png","vector-robot.png", "wheel-robot.png","dog-robot.png","flying-robot.png","excited-robot.png","invader-zim-robot.png","vector-robot.png"],
-}*/
-    $(".memory-info-container").click(function(){
-        $(".card").click(function(){
-            $(this).each("img");
-            console.log("cliked")
+$(document).ready(function() {
+    $("#memory-container").click(function() {
+        $(".card").click(function() {
+        $(this).setTimeout(() => {
+                
+            },5000);
+            
         });
     });
-    
-   
- //shuffle//
-/*$("#startButton").click(function(){
-    $("#memory-container").children();
-    $("div.card").each(function(){
-$(this).
 })
-});*/
-
-//start time//
-/*var i = 1;
-$("#startButton").click(function(e){
-setInterval(function(){
-    $("#time-remaining").html(i)
-
-    i++;
-     }, 1000 );
-    if (i === 60){
-        alert ("sorry, out of time");
-        clearInterval();
-    }  
-});
-
-$(".resetButton").click(function(e){
-    i = 0;
-})*/
-
-
-
-   
-
-});
-
-
-
-//win
-
-//loose
-
-
-
-
-
-
-  
-
