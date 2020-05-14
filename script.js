da09425adc64a5e3c6b3227562dd20899b253c79
Tess2019 <terese_burman@hotmail.com>
2020-05-14 11:14:03 +0000

    added onclick on button for multiple functions

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
 $(document).ready(function() {
     $("#memory-container").click(function() {
-        $(".card").manual();
-    
-         
-        });
-    });
+        $(".card").manual().each();
+});
+
+
+
 
     
-    var i = 60; 
+/*onclick="Display1(); Display2()";
 
-$("#startButton").click(function(e){
+$("#startButton").onclick(function(){
    setInterval(function(){
+       var i = 30;
       $("#time-remaining").html(i)
-      i-- ;
-      }, 1000);
+      i--;
+       }, 1000);
     if (i === 0 ){
-        clearInterval();
+        clearInterval("#startButton");
         alert("Time´s Up!");
-    } 
+        };
           
 });
     //resetTimer//
     $("#resetButton").click(function(e){
-        i = 0;
+        i = 30;
+});*/
+
+
+var Image = ["wheel-robot.png","dog-robot.png","flying-robot.png","excited-robot.png","invader-zim-robot.png","vector-robot.png", "wheel-robot.png","dog-robot.png","flying-robot.png","excited-robot.png","invader-zim-robot.png","vector-robot.png"];
+$("#startButton").click(function(){
+$(".card").select();
+$(this).trigger(Math.floor(Math.random() * Image.length));
+
 });
 
+//flips//
+var count = 0;
+$(".card").click(function(e){
+    $(this).each(function(){
+    $("#flips").html(count++);
+    });
+});
+$("#resetButton").click(function(e){
 
-var Image = ["wheel-robot.png","dog-robot.png","flying-robot.png","excited-robot.png","invader-zim-robot.png","vector-robot.png", "wheel-robot.png","dog-robot.png","flying-robot.png","excited-robot.png","invader-zim-robot.png","vector-robot.png"],
+}); 
 
-$(".card").attr("div",Image);
 
+});
 
 
 
