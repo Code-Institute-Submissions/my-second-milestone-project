ac1955cde396ccbceacac38c55148ebde39919d0
Tess2019 <terese_burman@hotmail.com>
2020-05-14 07:40:19 +0000

    starting on shuffle cards

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 $(document).ready(function() {
     $("#memory-container").click(function() {
-        $(".card").mouseover();
-        $(this).setTimeout(function(){
-            
-        }, 1000);
-        
-            
+        $(".card").manual();
+    
+         
         });
     });
+
+    
+    var i = 60; 
+
+$("#startButton").click(function(e){
+   setInterval(function(){
+      $("#time-remaining").html(i)
+      i-- ;
+      }, 1000);
+    if (i === 0 ){
+        clearInterval();
+        alert("Time´s Up!");
+    } 
+          
+});
+    //resetTimer//
+    $("#resetButton").click(function(e){
+        i = 0;
+});
+
+
+var Image = ["wheel-robot.png","dog-robot.png","flying-robot.png","excited-robot.png","invader-zim-robot.png","vector-robot.png", "wheel-robot.png","dog-robot.png","flying-robot.png","excited-robot.png","invader-zim-robot.png","vector-robot.png"],
+
+$(".card").attr("div",Image);
+
+
+
+
 
