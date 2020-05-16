b27b0cc18b77425be83b20b7ea7ba74cc3af338d
Tess2019 <terese_burman@hotmail.com>
2020-05-16 15:42:22 +0000

    starting a new random number calc.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
+
+   
     $("#memory-container").click(function() {
         $(".card").click(function(){
-            $("#card-back-"+this.id).hide();
+           $("#card-back-"+this.id).hide();
             $("#card-front-"+this.id).show();
-        });
-});
+       });
+    });
+
    //var i = 10;
 $("#startButton").click(function(){
 
@@ -15,14 +18,15 @@ $("#startButton").click(function(){
    if (i === 0 ){
        clearInterval(timer)
      };*/
-   var cardValue= [1,2,3,4,5,6,7,8,9,10,11,12],
-   const randomCard = cardValue[Math.floor(Math.random()* 12 + 3)];
+   
+    const numbers= [1,2,3,4,5,6,7,8,9,10,11,12];
+      $(".card").each(numbers, function(index, value){
+   numbers= [Math.floor(Math.random()* 12 + 3)];
 
-   $(".card").each(function(){
-       $(this).attr(randomCard);
-   });
-     });
-    
+      });
+  
+     
+})
     //resetTimer//
     $("#resetButton").click(function(){
         i = 30;
@@ -40,7 +44,6 @@ $(".card").click(function(){
 });
 
 });
-
 
 
 
