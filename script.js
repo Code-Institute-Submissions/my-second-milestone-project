e8979038e7f355f68989f83ad3f23106775fb598
Tess2019 <terese_burman@hotmail.com>
2020-05-27 13:00:32 +0000

    set callfunctions in top setup

--- script.js
+++ script.js
@@ -1,45 +1,52 @@
-$(document).ready(function() {
-$(".card").click(function() {
+$(document).ready(function () {
+  cardclick();
+  startbutton();
+  shuffle();
+  flipcards();
+  //click the card
+  function cardclick() {
+    $(".card").click();
     $(this).addClass(".card flipped");
-});
+  }
 
-$("#startButton").click(function(e) {
-//alert(this);//
-//timer start//
-     e.preventDefault();
-    let i = 10;
-    let TimeOut = setInterval(function() {
-      $("#time-remaining").html(i);
-      i--;
-      if (i === -1) {
-        alert("Time's Up, Try Again!");
-        clearInterval(TimeOut);
+  //start the timer and shuffle cards on start button
+  function startbutton() {
+    $("#startButton").click(function (e) {
+      //alert(this);//
+      //timer start//
+      e.preventDefault();
+      let count = 10;
+      let TimeOut = setInterval(function () {
+        $("#time-remaining").html(i);
+        count--;
+        if (count === -1) {
+          alert("Time's Up, Try Again!");
+          clearInterval(TimeOut);
+        }
+      }, 1000);
+    });
+  }
+
+  //cards loop//
+  function shuffle() {
+    $(".card").each(function(i) {
+      $(this).append("assets/images" + (++i) + ".png");
+      const images= ["wheel-robot.png","dog-robot.png","flying-robot.png","excited-robot.png","invader-zim-robot.png","vector-robot.png", "wheel-robot.png","dog-robot.png","flying-robot.png","excited-robot.png","invader-zim-robot.png","vector-robot.png"];
+      for (let i = 0; i < images.length; i++) {
+        // = Math.floor(Math.random() * images.length);
+        //startbutton();
+        console.log(images);
       }
-    }, 1000);
     });
+  }
+  //count the flips//
 
-
-//cards loop//
-$("#startButton").click(function(){
-    let cards = $(".cards");
-const Images= ["wheel-robot.png","dog-robot.png","flying-robot.png","excited-robot.png","invader-zim-robot.png","vector-robot.png", "wheel-robot.png","dog-robot.png","flying-robot.png","excited-robot.png","invader-zim-robot.png","vector-robot.png"];
-for( let i=0; i < Images.length; i++){
-  cards[i] = (Math.floor(Math.random()* Images.length));
-
-    // console.log(cards[i]);
-};
-});
-
-     //count the flips//
-let count = 0;
-  $(".card").click(function() {
-    $(this).each(function() {
-      $("#flips").html(count++);
+  function flipcards() {
+    let count = 0;
+    $(".card").click(function () {
+      $(this).each(function () {
+        $("#flips").html(count++);
+      });
     });
+  };
 });
-
-
-});
-
-
-
